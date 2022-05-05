6858ae712bd28c66aad12ca86e4117cba0ccfd96
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-05 17:52:04 -0400

    debug leap year exercise

--- notes.py
+++ notes.py
@@ -47,13 +47,27 @@
 
 
 
-############ Exercises #####################
+############ EXERCISES #####################
+#ODD OR EVEN
+# number = int(input("Which number do you want to check?"))
 
-number = int(input("Which number do you want to check?"))
+# if number % 2 == 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
 
-if number % 2 == 0:
-  print("This is an even number.")
+# LEAP YEAR
+year = input("Which year do you want to check?")
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
 else:
-  print("This is an odd number.")
+  print("Not leap year.")
   
 
