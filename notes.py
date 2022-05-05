44b00f9044726d48c6b2a0417054d251186732a0
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-05 17:49:32 -0400

    odd or even - solution

--- notes.py
+++ notes.py
@@ -45,11 +45,13 @@
 
 # mutate([1, 2, 3, 5, 8, 13])
 
+
+
 ############ Exercises #####################
 
-  number = int(input("Which number do you want to check?"))
+number = int(input("Which number do you want to check?"))
 
-if number % 2 = 0:
+if number % 2 == 0:
   print("This is an even number.")
 else:
   print("This is an odd number.")
